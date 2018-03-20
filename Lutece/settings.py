aabb4ad2555310153145adff13223b5daec2ea2d
LaPluses <xipersup@gmail.com>
2018-03-20 23:50:45 +0800

    Add requirements and modify settings.py

--- Lutece/settings.py
+++ Lutece/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Lutece.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Lutece Database',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT' : '3306'
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC+8'
 
 USE_I18N = True
 
