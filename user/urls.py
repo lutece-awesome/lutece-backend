c7566d5c12a81f4564b9f23320985968c94a8c61
LaPluses <xipersup@gmail.com>
2018-04-12 10:50:35 +0800

    Update user/

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.contrib.auth import authenticate
 from django.views.generic import TemplateView
+from . import views
 
 
 urlpatterns = [
     path( 'signin/' , TemplateView.as_view( template_name = 'signin.html'  ) , name = 'user_signin' ),
+    path( 'login/' , views.login , name = 'user_login' )
 ]
--- user/views.py
+++ user/views.py
@@ -4,3 +4,8 @@ from .models import user
 # Create your views here.
 
 
+def login(request):
+    if request.method == 'POST':
+        pass
+    else:
+        raise 
