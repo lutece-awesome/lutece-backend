5b77230e911ba2a5782de350000e335e7f84953d
unknown <xipersup@gmail.com>
2018-04-15 22:56:12 +0800

    Update submission

--- submission/urls.py
+++ submission/urls.py
@@ -3,5 +3,5 @@ from .views import submit_solution
 
 
 urlpatterns = [
-    path('submit/', submit_solution , name='submission-submit-solution'),
+    path('solution/', submit_solution , name='submit-solution'),
 ]
--- submission/views.py
+++ submission/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from annoying.functions import get_object_or_None
+from json import dumps
 # Create your views here.
 
 
 def submit_solution(request):
-    pass
+    status = {
+        'submission_id' : -1
+    }
+    try:
+        if request.method == 'POST':
+            problemid = request.POST.get( 'problemid' )
+            code = request.POST.get( 'code' )
+            if problemid == None or code == None:
+                raise ValueError( "Some solution info missed." )
+    finally:
+        return HttpResponse( dumps( status ) , content_type = 'application/json' )
