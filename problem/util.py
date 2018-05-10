8c626eebb852bbfd0fb530c15db77cf554af6097
LaPluses <xipersup@gmail.com>
2018-05-10 23:18:27 +0800

    fix bugs

--- problem/util.py
+++ problem/util.py
@@ -9,9 +9,16 @@ def get_user_problem_analysis( user , problem ):
     return 0
 
 def get_problem_analysis( problem ):
-    return ( len( { x.user for x in Submission.objects.filter( problem = problem , judge_status = 'Accepted' ) } ),
-             len( { x.user for x in Submission.objects.filter( problem = problem ) } ))
-
+    s = Submission.objects.filter( problem = problem ).order_by('submission_id')
+    ac_user = set()
+    _all = 0
+    for x in s:
+        if x.user.pk in ac_user:
+            continue
+        _all += 1
+        if x.judge_status == 'Accepted':
+            ac_user.add( x.user.pk )
+    return ( len( ac_user ) , _all )
 
 def get_search_url():
     return '/problem/search'
