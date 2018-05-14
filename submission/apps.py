962e53b0c57541687362009ab06121367914dbe1
LaPluses <xipersup@gmail.com>
2018-05-15 00:12:25 +0800

    fix

--- submission/apps.py
+++ submission/apps.py
@@ -1,8 +1,7 @@
 from django.apps import AppConfig
 from django.db.models.signals import pre_save
-from .tasks import init_push_waiting_submission
 
 class SubmissionConfig(AppConfig):
     name = 'submission'
     def ready( self ):
-        init_push_waiting_submission()
+        pass
