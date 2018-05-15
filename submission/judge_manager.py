94ed21d022449fcdce68f3b8687c05968ae05bb1
LaPluses <xipersup@gmail.com>
2018-05-15 23:28:45 +0800

    fix bugs

--- submission/judge_manager.py
+++ submission/judge_manager.py
@@ -1,4 +1,5 @@
 from multiprocessing.managers import BaseManager
+from Lutece.production import JUDGE_AUTH_KEY, JUDGE_PORT
 from sys import modules
 from queue import Queue
 from threading import Thread
@@ -14,7 +15,7 @@ class _manager:
         result_queue = Queue()
         QueueManager.register('get_task_queue', callable=lambda: task_queue)
         QueueManager.register('get_result_queue', callable=lambda: result_queue)
-        self.m = QueueManager(address=('0.0.0.0', 6106 ), authkey = b'772002')
+        self.m = QueueManager(address=('0.0.0.0', JUDGE_PORT ), authkey = JUDGE_AUTH_KEY )
         s = self.m.get_server()
         s.serve_forever()
         #self.m.start( signal.signal, (signal.SIGINT, signal.SIG_IGN) )
