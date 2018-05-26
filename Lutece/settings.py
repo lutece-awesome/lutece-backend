f685d624452549e77413a701b5b860ba52ee4819
LaPluses <xipersup@gmail.com>
2018-05-26 16:01:51 +0800

    Update

--- Lutece/settings.py
+++ Lutece/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_gravatar',
-    'markdownx',
     'gunicorn',
     # Lutece app
     'problem',
--- problem/models.py
+++ problem/models.py
@@ -1,16 +1,14 @@
 from django.db import models
-from markdownx.models import MarkdownxField
-from markdownx.utils import markdownify
 
 
 class Problem(models.Model):
     problem_id = models.AutoField(primary_key=True, db_index=True)
     title = models.CharField(
         max_length=256, db_index=True, default='Default Title', unique = True )
-    content = MarkdownxField( blank = True )
-    standard_input = MarkdownxField( blank = True )
-    standard_output = MarkdownxField( blank = True )
-    constraints = MarkdownxField( blank = True )
+    content = models.TextField( blank = True )
+    standard_input = models.TextField( blank = True )
+    standard_output = models.TextField( blank = True )
+    constraints = models.TextField( blank = True )
     resource = models.CharField(max_length=256, blank=True)
     note = models.TextField( blank = True )
     time_limit = models.PositiveIntegerField(default=2000)
@@ -20,22 +18,6 @@ class Problem(models.Model):
 
     def __str__(self):
         return self.title
-    
-    @property
-    def content_markdown(self):
-        return markdownify( self.content )
-    
-    @property
-    def standard_input_markdown(self):
-        return markdownify( self.standard_input )
-    
-    @property
-    def standard_output_markdown(self):
-        return markdownify( self.standard_output )
-    
-    @property
-    def constraints_markdown(self):
-        return markdownify( self.constraints )
 
     class Meta:
         ordering = ['problem_id']
