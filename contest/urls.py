5d218d24b4379e8a23d28cc9a7f1c9b1a546a429
LaPluses <xipersup@gmail.com>
2018-06-09 22:29:47 +0800

    Add contest_submission

--- contest/urls.py
+++ contest/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . views import get_contest_list, create_contest, edit_contest, update_contest, overview_contest, get_contest_problem, get_problem_list
+from . views import get_contest_list, create_contest, edit_contest, update_contest, overview_contest, get_contest_problem, get_problem_list, get_contest_submission
 
 urlpatterns = [
     path( 'list/<int:page>/' , get_contest_list  , name = 'contest-list' ),
@@ -8,5 +8,6 @@ urlpatterns = [
     path( 'update/<int:pk>/' , update_contest , name = 'contest-update' ),
     path( 'problem/<int:pk>/<int:index>/' , get_contest_problem , name = 'contest-problem' ),
     path( 'overview/<int:pk>/' , overview_contest , name = 'contest-overview' ),
+    path( 'submission/<int:pk>/<int:page>' , get_contest_submission , name = 'contest-submission' ),
     path( 'problem/list/<int:pk>/' , get_problem_list , name = 'contest-problem-list' )
 ]
