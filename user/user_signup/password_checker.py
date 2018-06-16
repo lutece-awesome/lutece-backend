f8614347fa007a57e156e30003f6e11e085e8b5f
LaPluses <xipersup@gmail.com>
2018-06-16 21:13:37 +0800

    Add the maxlength of password

--- user/user_signup/password_checker.py
+++ user/user_signup/password_checker.py
@@ -4,14 +4,14 @@ from re import compile, search
 def get_password_strength(password):
     """
         Return the report of password_strength as str list.
-        + password length should between 6 and 12 characters.
+        + password length should between 6 and 20 characters.
         + password should contain at least one lowercase letter, one uppercase letter, one digit.
     """
     errormsg = []
     try:
-        if len(password) < 6 or len(password) > 12:
+        if len(password) < 6 or len(password) > 20:
             errormsg.append(
-                'The length of password should bettween 6 and 12 characters.')
+                'The length of password should bettween 6 and 20 characters.')
         if compile('[a-z]').search(password) == None:
             errormsg.append(
                 'Password should contain at least one lowercase letter.')
