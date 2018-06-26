59340f03ae4f81d20c94fdfe78674e10a3677c18
LaPluses <xipersup@gmail.com>
2018-06-26 17:07:40 +0800

    Update

--- home/views.py
+++ home/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_render( request ):
-    return render( request , 'home/home.html' )
+    from user.models import User
+    top_user = User.objects.filter( show = True ).order_by( '-solved' )[:10]
+    return render( request , 'home/home.html' , {
+        'top_user' : top_user,
+    } )
