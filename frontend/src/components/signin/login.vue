4adec2588e68545e86ccf46c469f2786dc05f18b
LaPluses <xipersup@gmail.com>
2018-07-04 21:32:23 +0800

    Update

--- frontend/src/components/signin/login.vue
+++ frontend/src/components/signin/login.vue
@@ -3,7 +3,7 @@
     <div class="ui two column middle aligned very relaxed stackable segment grid" style = 'margin:0 auto;width:800px;'>
 
         <div class = 'column'>
-            <div class = 'ui form'>
+            <div class = 'ui form' v-bind:class = '{error : iserror}' >
                 <h4 style = 'text-align:center;' > <i class = 'sign in alternate icon'></i> LOG IN </h4>
                 <div class = 'field'>
                     <label> Username </label>
@@ -22,6 +22,7 @@
                 <div class = 'field'>
                     <FormButton buttonstyle = 'ui primary fluid button' fluid = 'true' msg = 'Login' :action = 'login' :error = 'loginerror' />
                 </div>
+                <ErrorMessage errorstyle = 'ui error message' header = 'LOGIN ERROR' msg = 'Username or Password Wrong'/>
             </div>
         </div>
 
@@ -31,7 +32,7 @@
 
         <div class="center aligned column">
             <div style = 'vertical-align:middle;'>
-                <FormButton buttonstyle = 'ui medium labeled icon positive button' icon = 'edit icon' msg = 'SIGN UP' />
+                <FormButton buttonstyle = 'ui medium labeled icon positive button' icon = 'edit icon' msg = 'SIGN UP' :action = 'signup' />
             </div>
         </div>
 
@@ -41,33 +42,33 @@
 
 <script>
     import FormButton from '@/components/basic/formbutton.vue'
+    import ErrorMessage from '@/components/basic/error.vue'
 
     export default {
         data: function(){
             return{
                 username : '',
                 password : '',
+                iserror : false,
             }
         },
         components:{
-            FormButton
+            FormButton,
+            ErrorMessage
         },
 
         methods:{
             login: function(){
-                
+                this.iserror = false;
             },
             loginerror: function( error ){
-                alert( error );
+                this.iserror = True;
             },
             signup: function(){
-                
+                this.$router.push( { name : 'Signup' });
             }
         }
     }
 </script>
 
-<style scoped>
-
-</style>
 
