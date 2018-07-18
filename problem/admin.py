0943d216b05006f0ab37dc9e99836f98228351eb
Zhu He <hezhu1688@gmail.com>
2018-07-18 21:29:15 +0800

    add the Textarea for samples

--- problem/admin.py
+++ problem/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import Problem, Sample, ProblemDiscussion
+from django import forms
 
+class SampleModelForm( forms.ModelForm ):
+    input_content = forms.CharField( widget=forms.Textarea )
+    output_content = forms.CharField( widget=forms.Textarea )
+    class Meta:
+        model = Sample
+        fields = '__all__'
 
 class SampleInline(admin.StackedInline):
+    form = SampleModelForm
     model = Sample
 
 class ProblemDiscussionInline(admin.StackedInline):
