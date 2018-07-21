d6e7939383902a288e2bc40f5671b98c8e0bed66
LaPluses <xipersup@gmail.com>
2018-07-22 00:51:34 +0800

    Update StatusDetail websocket backend API support

--- submission/consumers.py
+++ submission/consumers.py
@@ -1,34 +1,46 @@
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
-from .models import Submission
-
+from .models import Submission, Judgeinfo
+from json import dumps
+from .util import construct_websocketdata
 
 class StatusDetailConsumer( WebsocketConsumer ):
 
-    def connect( self ):
-        self.pk = self.scope['url_route']['kwargs']['pk']
-        self.group_name = 'StatusDetail_%d' % self.pk
-        self.init = False
-        self.submission = Submission.objects.get( self.pk )
-
+    async def connect( self ):
+        self.submission = Submission.objects.get( self.scope['url_route']['kwargs']['pk'] )
+        self.group_name = 'StatusDetail_%d' % self.submission.pk
+        '''
+            Auth
+        '''
         if not self.scope['user'].has_perm( 'problem.view_all' ) and not self.submission.problem.visible:
             raise RuntimeError( 'Permission Denied' )
 
-        async_to_sync(self.channel_layer.group_add)(
+        await async_to_sync(self.channel_layer.group_add)(
             self.group_name,
             self.channel_name
         )
-        self.accept()
+        await self.accept()
+        self.init()
+
+    '''
+        Send the judge_result that has been created before
+    '''
+
+    async def init( self ):
+        s = Judgeinfo.objects.filter( submission = self.submission )
+        self.send( text_data = construct_websocketdata( result = self.submission.judge_status ,  judge =  [ {
+            'timecost' : each.time_csot,
+            'memorycost' : each.memory_cost,
+            'result': each.result,
+            'case': each.case
+        } for each in s ] ) )
     
-    def disconnect( self , close_code ):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect( self , close_code ):
+        await async_to_sync(self.channel_layer.group_discard)(
             self.group_name,
             self.channel_name
         )
 
-    def initialize( self ):
-        if not self.init:
-            self.init = True
-
-    def update_result( self , message ):
-        pass
+    async def update_result( self , event ):
+        data = event['data']
+        self.send( text_data = data )
