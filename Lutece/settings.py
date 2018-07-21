9ab807ae321ecf14a1a98613b73daf61c83d6b15
LaPluses <xipersup@gmail.com>
2018-07-22 03:51:30 +0800

    UPDATE README.MD

--- Lutece/settings.py
+++ Lutece/settings.py
@@ -80,10 +80,10 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("redis-server-name", 6379)],
         },
     },
 }
