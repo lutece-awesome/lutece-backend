b59cf1b1a67f62ffcabbd7377acf5a79d85c9886
LaPluses <xipersup@gmail.com>
2018-07-25 00:16:26 +0800

    Add userType back-end support

--- user/permission_view.py
+++ user/permission_view.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import permission_required
 from json import dumps
-from .group import get_user_control_permission, get_user_group
+from .group import get_user_control_permission
 
 @permission_required( 'user.set_normal_user' )
 def set_normal_user( request ):
@@ -9,7 +9,7 @@ def set_normal_user( request ):
     from .group import Group
     ret = { 'status' : True, }
     user = User.objects.get( pk = int(request.POST.get( 'user' )) )
-    if len( get_user_control_permission( get_user_group( request.user.group ) , user ) ) == 0:
+    if len( get_user_control_permission( Group.get_user_group( request.user.group ) , user ) ) == 0:
         raise RuntimeError( 'Permission Denied' )
     user.set_group( Group.NORMAL_USER )
     return HttpResponse( dumps( ret ) , content_type = 'application/json' )
@@ -20,7 +20,7 @@ def set_normal_admin( request ):
     from .group import Group
     ret = { 'status' : True, }
     user = User.objects.get( pk = int(request.POST.get( 'user' )) )
-    if len( get_user_control_permission( get_user_group( request.user.group ) , user ) ) == 0:
+    if len( get_user_control_permission( Group.get_user_group( request.user.group ) , user ) ) == 0:
         raise RuntimeError( 'Permission Denied' )
     user.set_group( Group.NORMAL_ADMIN )
     return HttpResponse( dumps( ret ) , content_type = 'application/json' )
@@ -31,7 +31,7 @@ def set_super_admin( request ):
     from .group import Group
     ret = { 'status' : True, }
     user = User.objects.get( pk = int(request.POST.get( 'user' )) )
-    if len( get_user_control_permission( get_user_group( request.user.group ) , user ) ) == 0:
+    if len( get_user_control_permission( Group.get_user_group( request.user.group ) , user ) ) == 0:
         raise RuntimeError( 'Permission Denied' )
     user.set_group( Group.SUPER_ADMIN )
     return HttpResponse( dumps( ret ) , content_type = 'application/json' )
