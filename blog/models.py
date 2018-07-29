4929bb8e2f5f5a9c557442f25830e62b428682c3
LaPluses <xipersup@gmail.com>
2018-07-29 22:05:39 +0800

    Update

--- blog/models.py
+++ blog/models.py
@@ -13,6 +13,8 @@ class Blog( models.Model ):
     create_time = models.DateTimeField ( default = timezone.now )
     slug = models.CharField( blank = True , max_length = 256 )
     view = models.IntegerField( default = 0 )
+    vote = models.IntegerField( default = 0 )
+    disable = models.BooleanField( default = False )
 
     def __str__(self):
         return self.title
@@ -21,10 +23,15 @@ class Blog( models.Model ):
         self.slug = uuslug( self.title, instance = self )
         super( Blog , self ).save( * args , ** kwargs )
 
+    class Meta:
+        permissions = (
+            ('view_all' , 'Can view all blogs'),
+        )
+
 class BlogVoteUser( models.Model ):
     user = models.OneToOneField( User , null = True , on_delete = models.SET_NULL )
     blog = models.OneToOneField( Blog , null = True , on_delete = models.SET_NULL )
-    vote = models.IntegerField( default = 0 )
+    vote = models.BooleanField( default = True )
 
 class BlogDiscussion( Discussion ):
     blog = models.ForeignKey( Blog  , null = True , on_delete = models.SET_NULL)
