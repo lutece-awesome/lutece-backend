ea0c120c6a6593d0615245ed1876eea560e85c40
Zhu He <hezhu1688@gmail.com>
2018-08-07 23:31:17 +0800

    remove unused files and codes

--- discussion/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .problem_discussion_views import problem_discussion_show, problem_discussion_reply
-from .util_views import discussion_change_visibility
-
-urlpatterns = [
-    path('show/<int:pk>/', problem_discussion_show, name='discussion-problem-show'),
-    path('reply/<int:pk>/', problem_discussion_reply, name='discussion-problem-reply'),
-    path('visibility/', discussion_change_visibility, name='discussion-change-visibility'),
-]
