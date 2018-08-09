86822c38f55bc6133018ced33c2b02fc8ab4f55d
LaPluses <xipersup@gmail.com>
2018-08-10 00:38:12 +0800

    Update upload interface

--- utils/form.py
+++ utils/form.py
@@ -2,3 +2,10 @@ from django import forms
 
 class UploadImageForm( forms.Form ):
     image = forms.ImageField( required = True )
+
+
+    def clean( self ):
+        cleaned_data = super().clean()
+        image = cleaned_data.get( 'image' )
+        if image and image.size > 2 * 1024 * 1024:
+            self.add_error( 'image' , 'Image size can not exceed 2mb' )
