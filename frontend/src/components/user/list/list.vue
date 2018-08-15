e1f89acd7dac0c74c67319d794e28312e363e14d
LaPluses <xipersup@gmail.com>
2018-08-14 22:40:07 -0700

    Update

--- frontend/src/components/user/list/list.vue
+++ frontend/src/components/user/list/list.vue
@@ -71,20 +71,62 @@
 			<v-flex
 				v-for = "( each , index ) in userItem" 
 				:key = "index"
-				xs4>
+				xs12
+				sm6
+				md4
+				lg3>
 			<v-card 
 				hover 
 				:to = "{ name: 'UserDetail' , params: {username: each.username } }"
-				class = "mb-4 mr-4" >
-				<v-card-text>
-					<v-avatar>
-						<img :src=  "each.gravataremail" />
-					</v-avatar>
-				</v-card-text>
-				<v-divider class = "mt-2 mb-2" />
-				<v-card-text class = "about" >
-					{{ each.about }}
-				</v-card-text>
+				class = "mb-4 mr-4 pt-4" >
+				<v-layout row wrap>
+					<v-flex xs4>
+						<v-card-media
+							class = "card-avatar"
+							height = "64px"
+							:src=  "each.gravataremail"
+							contain
+						>
+						</v-card-media>
+					</v-flex>
+					<v-flex xs8>
+						<v-card-tile>
+							<div class = "ml-2" >
+								<div
+									class = "headline mb-1"
+									style = "text-overflow: ellipsis;overflow: hidden;">
+									{{ each.displayName }}
+								</div>
+								<div v-if = "each.school" class = "mb-1" >
+									<v-icon class="mdi-18px">mdi-school</v-icon>
+									<span class = "ml-1"> {{ each.school }} </span>
+								</div>
+								<div v-if = "each.company" class = "mb-1" >
+									<v-icon class="mdi-18px">mdi-domain</v-icon>
+									<span class = "ml-1"> {{ each.company }} </span>
+								</div>
+								<div v-if = "each.location" class = "mb-1" >
+									<v-icon class="mdi-18px">mdi-map-marker</v-icon>
+									<span class = "ml-1"> {{ each.location }} </span>
+								</div>
+							</div>
+						</v-card-tile>
+					</v-flex>
+					<v-flex xs12>
+						<v-divider class = "mt-2" />
+					</v-flex>
+					<v-flex xs12>
+						<v-list one-line>
+							<v-list-tile>
+								<v-list-tile-content>
+									<v-list-tile-sub-title>
+										<span class = "text--primary"> {{ each.about }} </span>
+									</v-list-tile-sub-title>
+								</v-list-tile-content>
+							</v-list-tile>
+						</v-list>
+					</v-flex>
+				</v-layout>
 			</v-card>
 			</v-flex>
 		</v-layout>
@@ -109,8 +151,4 @@ export default {
 
 
 <style scoped>
-	.about {
-		text-overflow: ellipsis;
-		overflow: hidden;
-	}
 </style>
