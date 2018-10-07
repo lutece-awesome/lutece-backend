2da534e94c2ec6c50fe4b467b0e8c6bc3bb06523
LaPluses <xipersup@gmail.com>
2018-10-08 02:25:35 +0800

    Add the max length constraint

--- problem/baseproblem/constant.py
+++ problem/baseproblem/constant.py
@@ -1,20 +1,20 @@
 # The max length limitation of problem title
-MAX_TITLE_LENGTH = 128
+MAX_TITLE_LENGTH = 256
 
 # The max length limitation of problem content
 MAX_CONTENT_LENGTH = 655535
 
 # The max length limitation of problem resources
-MAX_RESOURCES_LENGTH = 128
+MAX_RESOURCES_LENGTH = 256
 
 # Te max length limitation of problem constraints
-MAX_CONSTRAINTS_LENGTH = 1024
+MAX_CONSTRAINTS_LENGTH = 8192
 
 # The max length limitation of problem note
-MAX_NOTE_LENGTH = 1024
+MAX_NOTE_LENGTH = 8192
 
 # The max length limitation of problem standard input
-MAX_STANDARD_INPUT_LENGTH = 2048
+MAX_STANDARD_INPUT_LENGTH = 8192
 
 # The max length limitation of problem standard output
-MAX_STANDARD_OUTPUT_LENGTH = 2048
+MAX_STANDARD_OUTPUT_LENGTH = 8192
