6a662a8096b0db7fce2bd8189aedbfdb984cf7b7
LaPluses <xipersup@gmail.com>
2018-10-08 02:30:42 +0800

    Update

--- problem/baseproblem/constant.py
+++ problem/baseproblem/constant.py
@@ -2,19 +2,19 @@
 MAX_TITLE_LENGTH = 256
 
 # The max length limitation of problem content
-MAX_CONTENT_LENGTH = 655535
+MAX_CONTENT_LENGTH = 32768
 
 # The max length limitation of problem resources
 MAX_RESOURCES_LENGTH = 256
 
 # Te max length limitation of problem constraints
-MAX_CONSTRAINTS_LENGTH = 8192
+MAX_CONSTRAINTS_LENGTH = 4096
 
 # The max length limitation of problem note
 MAX_NOTE_LENGTH = 8192
 
 # The max length limitation of problem standard input
-MAX_STANDARD_INPUT_LENGTH = 4096
+MAX_STANDARD_INPUT_LENGTH = 2048
 
 # The max length limitation of problem standard output
-MAX_STANDARD_OUTPUT_LENGTH = 4096
+MAX_STANDARD_OUTPUT_LENGTH = 2048
