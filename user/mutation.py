6b60facbee9ab044e21ee74f1f262da7aa9b6de8
LaPluses <xipersup@gmail.com>
2019-03-16 23:32:20 +0800

    [Contest]: Optmize the ranking query.

--- user/mutation.py
+++ user/mutation.py
@@ -5,6 +5,7 @@ from graphql import ResolveInfo
 from graphql_jwt.decorators import login_required
 from graphql_jwt.mixins import RefreshMixin, JSONWebTokenMixin
 from graphql_jwt.shortcuts import get_token, get_payload, get_user_by_payload
+from graphql_jwt import Refresh
 
 from user.attachinfo.models import AttachInfo
 from user.form import UserLoginForm, UserSignupForm, UserAttachInfoUpdateForm
@@ -36,7 +37,7 @@ class UserLogin(graphene.Mutation):
             raise RuntimeError(login_form.errors.as_json())
 
 
-class UserTokenRefresh(RefreshMixin, graphene.Mutation):
+class UserTokenRefresh(Refresh):
     permission = GenericScalar()
     user = graphene.Field(UserType)
 
